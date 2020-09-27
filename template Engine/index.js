fbde844347c7f26fd11e6333ad706b63ef9cee35
Nishant Gautam <eddienishant@gmail.com>
2020-09-27 12:56:13 +0530

    Basic Template Engine

---
+++ template Engine/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const port = 8000;
+const app = express()
+const path = require('path')
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/',function(req, res) {
+    return res.render('index')
+})
+
+
+
+
+
+
+
+
+
+app.listen(port,function(err) {
+    if(err) {
+        console.log(`There is an error: ${err}`)
+    }
+    console.log(`The Server is running on port ${port}`)
+})
