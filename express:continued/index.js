67871662f80c43df06cb88ce04a7cad5621dcd87
Nishant Gautam <eddienishant@gmail.com>
2020-09-28 16:06:22 +0530

    task 1 completed

--- express:continued/index.js
+++ express:continued/index.js
@@ -7,8 +7,38 @@ const app = express();
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+
+var contactList = [
+    {
+        name:"Nishant",
+        phone:"999999"
+    },
+    {
+        name:"kalu",
+        phone:"99999"
+    },
+    {
+        name:"Manu",
+        phone:"9999"
+
+    }
+    
+]
+
+
 app.get('/', function(req, res) {
-    return res.render('index')
+    return res.render('index', {
+        title:"My Contact List",
+        contact_list: contactList
+    })
+})
+
+app.get('/practice' , function(req, res) {
+    return res.render('practice')
+})
+
+app.post('/create-contact', function(req, res) {
+    return res.redirect('/practice')
 })
 
 
